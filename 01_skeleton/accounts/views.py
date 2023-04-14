00bf887253ca9b1efb0d5a5a58f6b95c52f4145e
rlaehd12 <dnpffldxjs12@gmail.com>
2023-04-14 14:25:09 +0900

    signup

--- 01_skeleton/accounts/views.py
+++ 01_skeleton/accounts/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
-from .forms import UserChangeForm, UserCreationForm
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
-def login(request):
+def login(request):  #GET, POST
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
@@ -17,14 +17,27 @@ def login(request):
     context = {'form': form}
     return render(request, 'accounts/login.html', context)
 
-def logout(request):
+
+def logout(request):  #POST
     if request.method == "POST":
         auth_logout(request)
         return redirect('movies:index')
     return redirect('movies:index')
 
-def signup(request):
-    pass
+
+def signup(request):  #GET POST
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('movies:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {'form': form}
+    return render(request, 'accounts/signup.html', context)
+
+
 def delete(request):
     pass
 def update(request):
