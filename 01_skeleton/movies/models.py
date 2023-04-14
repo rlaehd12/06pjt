4b7294c44f223d98f883ba5070717fb1c4d27665
rlaehd12 <dnpffldxjs12@gmail.com>
2023-04-14 16:53:11 +0900

    end

--- 01_skeleton/movies/models.py
+++ 01_skeleton/movies/models.py
@@ -14,7 +14,7 @@ class Movie(models.Model):
 
 class Comment(models.Model):
     content = models.CharField(max_length=100)
-    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
