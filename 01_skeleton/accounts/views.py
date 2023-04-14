11caa560a7bcf46cc88fa25ddd4ce1dc3e15c9cc
rlaehd12 <dnpffldxjs12@gmail.com>
2023-04-14 13:49:24 +0900

    login and templates

--- 01_skeleton/accounts/views.py
+++ 01_skeleton/accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import UserChangeForm, UserCreationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('movies:index')
+    else:
+        form = AuthenticationForm()
+
+    context = {'form': form}
+    return render(request, 'accounts/login.html', context)
+
+def logout(request):
+    pass
+def signup(request):
+    pass
+def delete(request):
+    pass
+def update(request):
+    pass
+def password(request):
+    pass
+def profile(request):
+    pass
+def follow(request):
+    pass
